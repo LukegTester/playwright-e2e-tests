39d3defb779343830863bd4aa78e6a64e2348f13
Luke.tester <lukeg.tester@gmail.com>
2024-06-21 09:27:37 +0200

    test: negative test scenario for GAD-R02-01 req

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -3,23 +3,46 @@ import { WelcomePage } from '../src/pages/welcome.page';
 import { testUser1 } from '../src/test-data/user.data';
 import { expect, test } from '@playwright/test';
 
-test(
-  'Verify login with correct credentials',
-  { tag: '@GAD-R02-01' },
-  async ({ page }) => {
-    // Arrange
-    const loginPage = new LoginPage(page);
-    const userEmail = testUser1.userEmail;
-    const userPassword = testUser1.userPassword;
+test.describe('Verify login', () => {
+  test(
+    'Verify login with correct credentials',
+    { tag: '@GAD-R02-01' },
+    async ({ page }) => {
+      // Arrange
+      const loginPage = new LoginPage(page);
+      const userEmail = testUser1.userEmail;
+      const userPassword = testUser1.userPassword;
 
-    // Act
-    await loginPage.goto();
-    await loginPage.login(userEmail, userPassword);
+      // Act
+      await loginPage.goto();
+      await loginPage.login(userEmail, userPassword);
 
-    const welcomePage = new WelcomePage(page);
-    const title = await welcomePage.title();
+      const welcomePage = new WelcomePage(page);
+      const title = await welcomePage.title();
 
-    // Assert
-    expect(title).toContain('Welcome');
-  },
-);
+      // Assert
+      expect(title).toContain('Welcome');
+    },
+  );
+  test(
+    'Reject login with incorrect credentials - password',
+    { tag: '@GAD-R02-01' },
+    async ({ page }) => {
+      // Arrange
+      const loginPage = new LoginPage(page);
+      const userEmail = testUser1.userEmail;
+      const userPassword = 'incorrectPassword';
+
+      // Act
+      await loginPage.goto();
+      await loginPage.login(userEmail, userPassword);
+
+      // Assert
+      const loginError = loginPage.loginError;
+      await expect.soft(loginError).toHaveText('Invalid username or password');
+
+      const title = await loginPage.title();
+      expect.soft(title).toContain('Login');
+    },
+  );
+});

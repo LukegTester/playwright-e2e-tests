6630491ab8967742cbf887f3b27f4fe61c6df115
Luke.tester <lukeg.tester@gmail.com>
2024-06-24 14:59:47 +0200

    test: register page test

---
+++ tests/register.spec.ts
@@ -0,0 +1,41 @@
+import { LoginPage } from '../src/pages/login.page';
+import { RegisterPage } from '../src/pages/register.page';
+import { WelcomePage } from '../src/pages/welcome.page';
+import { expect, test } from '@playwright/test';
+
+test.describe('Verify register', () => {
+  test(
+    'Verify register with correct data',
+    { tag: ['@GAD-R03-01', '@GAD-R03-02', '@GAD-R03-03'] },
+    async ({ page }) => {
+      // Arrange:
+      const registerPage = new RegisterPage(page);
+      const firstName = 'Janina';
+      const lastName = 'Nowak';
+      const email = `jntest${new Date().getTime()}@test.test1`;
+      const password = 'test12134';
+
+      const expectedMessage = 'User created';
+
+      // Act
+      await page.goto(registerPage.url);
+      await registerPage.register(firstName, lastName, email, password);
+
+      // Assert 1
+      await expect.soft(registerPage.alertPopUp).toHaveText(expectedMessage);
+
+      const loginPage = new LoginPage(page);
+      await loginPage.waitForPageToLoadUrl();
+      const titleLogin = await loginPage.title();
+      expect(titleLogin).toContain('Login');
+
+      // Assert 2
+      await loginPage.login(email, password);
+
+      const welcomePage = new WelcomePage(page);
+      const titleWelcome = await welcomePage.title();
+
+      expect(titleWelcome).toContain('Welcome');
+    },
+  );
+});

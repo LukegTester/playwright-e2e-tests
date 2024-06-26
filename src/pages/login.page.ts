f626175f7fa8e47b6d6783a85cb86f3b24e4930c
Luke.tester <lukeg.tester@gmail.com>
2024-06-26 15:19:56 +0200

    chore: login user model

--- src/pages/login.page.ts
+++ src/pages/login.page.ts
@@ -1,3 +1,4 @@
+import { LoginUser } from '../models/user.model';
 import { BasePage } from './base.page';
 import { Page } from '@playwright/test';
 
@@ -11,6 +12,16 @@ export class LoginPage extends BasePage {
   async login(userEmail: string, userPassword: string): Promise<void> {
     await this.page.getByPlaceholder('Enter User Email').fill(userEmail);
     await this.page.getByPlaceholder('Enter Password').fill(userPassword);
+    await this.page.getByRole('button', { name: 'LogIn' }).click();
+  }
+
+  async loginNew(loginUserData: LoginUser): Promise<void> {
+    await this.page
+      .getByPlaceholder('Enter User Email')
+      .fill(loginUserData.userEmail);
+    await this.page
+      .getByPlaceholder('Enter Password')
+      .fill(loginUserData.userPassword);
     await this.page.getByRole('button', { name: 'LogIn' }).click();
   }
 }

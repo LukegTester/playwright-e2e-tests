ee756e04cfbcac0754262c9cf1f14139499b1b2d
Luke.tester <lukeg.tester@gmail.com>
2024-07-03 09:27:10 +0200

    test: article page test implementation

---
+++ tests/articles.spec.ts
@@ -0,0 +1,39 @@
+import { ArticlePage } from '../src/pages/article.page';
+import { ArticlesPage } from '../src/pages/articles.page';
+import { LoginPage } from '../src/pages/login.page';
+import { testUser1 } from '../src/test-data/user.data';
+import { AddArticlesView } from '../src/views/add-articles.view';
+import { expect, test } from '@playwright/test';
+
+test.describe('Verify articles', () => {
+  test(
+    'Verify creating article with mandatory fields',
+    { tag: '@GAD-R04-01' },
+    async ({ page }) => {
+      // Arrange
+      const loginPage = new LoginPage(page);
+      await loginPage.goto();
+      await loginPage.login(testUser1);
+
+      const articlesPage = new ArticlesPage(page);
+      await articlesPage.goto();
+
+      // Act
+      await articlesPage.addArticleButtonLogged.click();
+      const addArticlesView = new AddArticlesView(page);
+      await expect.soft(addArticlesView.header).toBeVisible();
+
+      const articleTitle = 'test title';
+      const articleBody = 'test body';
+
+      await addArticlesView.titleInput.fill(articleTitle);
+      await addArticlesView.bodyInput.fill(articleBody);
+      await addArticlesView.saveButton.click();
+
+      // Assert
+      const articlePage = new ArticlePage(page);
+      await expect(articlePage.articleTitle).toHaveText(articleTitle);
+      await expect(articlePage.articleBody).toHaveText(articleBody);
+    },
+  );
+});

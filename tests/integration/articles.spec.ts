c53bc4fadf343b9e0886483adc487a6b1f695843
Luke.tester <lukeg.tester@gmail.com>
2024-09-16 11:39:04 +0200

    chore: backend communication validation for articles

--- tests/integration/articles.spec.ts
+++ tests/integration/articles.spec.ts
@@ -1,11 +1,11 @@
-import { RESPONSE_TIMEOUT } from '@_pw-config';
 import { prepareRandomArticle } from '@_src/factories/article.factory';
 import { expect, test } from '@_src/fixtures/merge.fixtures';
+import { waitForResponse } from '@_src/utils/wait.util';
 
 test.describe('Verify articles', () => {
   test(
     'article is not created without mandatory fields - title not provided',
-    { tag: ['@GAD-R04-01', '@logged'] },
+    { tag: ['@GAD-R04-01', '@GAD-R07-03', '@logged'] },
     async ({ addArticlesView, page }) => {
       // Arrange
       const expectedMessage = 'Article was not created';
@@ -14,9 +14,7 @@ test.describe('Verify articles', () => {
       const articleData = prepareRandomArticle();
       articleData.articleTitle = '';
 
-      const responsePromise = page.waitForResponse('/api/articles', {
-        timeout: RESPONSE_TIMEOUT,
-      });
+      const responsePromise = waitForResponse(page, '/api/articles');
 
       // Act
       await addArticlesView.createArticle(articleData);
@@ -29,47 +27,59 @@ test.describe('Verify articles', () => {
   );
   test(
     'article is not created without mandatory fields - body not provided',
-    { tag: ['@GAD-R04-01', '@logged'] },
-    async ({ addArticlesView }) => {
+    { tag: ['@GAD-R04-01', '@GAD-R07-03', '@logged'] },
+    async ({ addArticlesView, page }) => {
       // Arrange
       const expectedMessage = 'Article was not created';
+      const expectedResponseCode = 422;
 
       const articleData = prepareRandomArticle();
       articleData.articleBody = '';
+      const responsePromise = waitForResponse(page, '/api/articles');
 
       // Act
       await addArticlesView.createArticle(articleData);
+      const response = await responsePromise;
 
       // Assert
       await expect(addArticlesView.alertPopup).toHaveText(expectedMessage);
+      expect(response.status()).toBe(expectedResponseCode);
     },
   );
   test.describe('Title length', () => {
     test(
       'article should not be created when title exceed 128 signs',
       { tag: ['@GAD-R04-02', '@logged'] },
-      async ({ addArticlesView }) => {
+      async ({ addArticlesView, page }) => {
         // Arrange
         const expectedMessage = 'Article was not created';
+        const expectedResponseCode = 422;
 
         const articleData = prepareRandomArticle(129);
+        const responsePromise = waitForResponse(page, '/api/articles');
 
         // Act
         await addArticlesView.createArticle(articleData);
+        const response = await responsePromise;
 
         // Assert
         await expect(addArticlesView.alertPopup).toHaveText(expectedMessage);
+        expect(response.status()).toBe(expectedResponseCode);
       },
     );
     test(
       'create article with title having 128 signs',
       { tag: ['@GAD-R04-02', '@logged'] },
-      async ({ addArticlesView }) => {
+      async ({ addArticlesView, page }) => {
         // Arrange
         const articleData = prepareRandomArticle(128);
+        const expectedResponseCode = 201;
+
+        const responsePromise = waitForResponse(page, '/api/articles');
 
         // Act
         const articlePage = await addArticlesView.createArticle(articleData);
+        const response = await responsePromise;
 
         // Assert
         await expect(articlePage.articleTitle).toHaveText(
@@ -78,6 +88,7 @@ test.describe('Verify articles', () => {
         await expect(articlePage.articleBody).toHaveText(
           articleData.articleBody,
         );
+        expect(response.status()).toBe(expectedResponseCode);
       },
     );
   });
